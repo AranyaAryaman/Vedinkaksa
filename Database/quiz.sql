4a65152de3720064ebb7cca14af67fdbdd5f5053
AranyaAryaman <aryamanaranya@gmail.com>
2021-04-09 22:26:45 +0530

    Database Files Updated - Project Completed

--- Database/quiz.sql
+++ Database/quiz.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.5deb2
+-- version 5.1.0
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost:3306
--- Generation Time: Mar 25, 2021 at 04:01 PM
--- Server version: 8.0.23-0ubuntu0.20.04.1
--- PHP Version: 7.4.3
+-- Host: localhost
+-- Generation Time: Apr 09, 2021 at 09:07 AM
+-- Server version: 10.5.9-MariaDB-1
+-- PHP Version: 7.4.15
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -21,6 +20,35 @@ SET time_zone = "+00:00";
 --
 -- Database: `quiz`
 --
+CREATE DATABASE IF NOT EXISTS `quiz` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `quiz`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `quiz_questions`
+--
+
+DROP TABLE IF EXISTS `quiz_questions`;
+CREATE TABLE IF NOT EXISTS `quiz_questions` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `question` varchar(111) NOT NULL,
+  `option1` varchar(100) NOT NULL,
+  `option2` varchar(100) NOT NULL,
+  `option3` varchar(100) NOT NULL,
+  `option4` varchar(100) NOT NULL,
+  `correct_answer` varchar(5) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `question` (`question`)
+) ENGINE=InnoDB AUTO_INCREMENT=39 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `quiz_questions`
+--
+
+INSERT INTO `quiz_questions` (`id`, `question`, `option1`, `option2`, `option3`, `option4`, `correct_answer`) VALUES
+(37, 'hi', 'oo', 'ii', 'yy', 'ss', '1'),
+(38, 'Who was the first president of India?', 'Pt. JawaharLal Nehru', 'Sardar Vallabhbhai Patel ', 'Dr. Rajendra Prasad', 'Mahatma Gandhi', '3');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
